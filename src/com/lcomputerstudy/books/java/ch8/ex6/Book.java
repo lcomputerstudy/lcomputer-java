9624804286b084aa59ed0b6e644ca28eb274003f
LcomputerStudy <lcomputerstudy@naver.com>
2022-09-20 15:58:08 +0900

    접근 제한자

---
+++ src/com/lcomputerstudy/books/java/ch8/ex6/Book.java
@@ -0,0 +1,51 @@
+package com.lcomputerstudy.books.java.ch8.ex6;
+
+public class Book {
+	public String title;
+	private String author;
+	private int price;
+	private int dollarPrice;
+	int edition;
+	protected float tax;
+	
+	void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+		setDollarPrice(price);
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	private void setDollarPrice(int price) {
+		dollarPrice = (int)(price / 1000f);
+	}
+	
+	public int getDollarPrice() {
+		return dollarPrice;
+	}
+	
+	protected void setTax(float tax) {
+		this.tax = tax;
+	}
+	
+	protected float getTax() {
+		return tax;
+	}
+}
