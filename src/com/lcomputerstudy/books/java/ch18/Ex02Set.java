c8cba76d3ddbf519acb22628186fdd1d7969ab0c
LcomputerStudy <lcomputerstudy@naver.com>
2022-10-24 21:48:03 +0900

    Set

--- src/com/lcomputerstudy/books/java/ch18/Ex02Set.java
+++ src/com/lcomputerstudy/books/java/ch18/Ex02Set.java
@@ -1,73 +1,149 @@
 package com.lcomputerstudy.books.java.ch18;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Ex02Set {
 
 	public static void main(String[] args) {
-		String[] subject = {"자바", "파이선", "알고리즘", "자료구조", "자바스크립트", "데이터베이스", "스프링"};
+		// 사용자로 부터 입력 받은 과목들이라고 가정합니다.
+		String[] arrSubjects = { "자바", "파이선", "자바", "알고리즘", "스프링", "자료구조", "자바스크립트", "데이터베이스", "스프링", "스프링", "C", "람다" };
+		System.out.println(Arrays.toString(arrSubjects));
+		System.out.println();
 		
-		Student s1 = new Student("A", new ArrayList<>());
-		s1.addBook(subject[0]);
-		s1.addBook(subject[1]);
-		Student s2 = new Student("B", new HashSet<>());
-		s2.addBook(subject[0]);
-		s2.addBook(subject[2]);
-		Student s3 = new Student("C", new HashSet<>());
-		s2.addBook(subject[0]);
-		s3.addBook(subject[3]);
-		s3.addBook(subject[4]);
+		Student s1 = new Student("A");
+		s1.addSubject("자바");
+		s1.addSubject("알고리즘");
+		s1.addSubject("파이썬");
+		s1.addSubject("스프링");
+		Student s2 = new Student("B");
+		s2.addSubject("자바");
+		s2.addSubject("데이터베이스");
+		s2.addSubject("스프링");
+		Student s3 = new Student("C");
+		s3.addSubject("자바");
+		s3.addSubject("스프링");
+		s3.addSubject("자바스크립트");
 		
-		// 학생들이 대출중인 책 모두 출력
-		Collection<String> books = new HashSet<>();
-		books.addAll(s1.getBooks());
-		books.addAll(s2.getBooks());
-		books.addAll(s3.getBooks());
-		System.out.println(books);
+		Course course = new Course();
+		course.setStudents(s1, s2, s3);
 		
-		// 아무도 수강중이지 않은 과목
-		Collection<String> duplList = new ArrayList<>();
-		Collections.addAll(duplList, subject);
-		Collection<String> duplSet = new HashSet<>(duplList);
-		duplSet.removeAll(books);
-		System.out.println(duplSet);
+		System.out.println("- 모든 과목 -");
+		course.setSubjects(arrSubjects);
+		Set<String> subjects = course.getSubjects();
+		Course.print(subjects);
 		
-		// 모든 학생이 수강중인 과목들 retainAll
+		System.out.println("- 정렬된 과목 -");
+		List<String> oSubjects = course.getOrderedSubjects();
+		Course.print(oSubjects);
 		
+		System.out.println("- 학생들이 수강 중인 모든 과목 (합집합) -");
+		Set<String> rSubjects = course.getRegisteredSubjects();
+		Course.print(rSubjects);
+		
+		System.out.println("- 모든 학생들이 수강 중인 과목 (교집합)");
+		Set<String> bSubjects = course.getBasicSubjects();
+		Course.print(bSubjects);
+		
+		System.out.println("- 모든 학생들이 수강 중이지 않은 과목 (여집합)");
+		Set<String> cSubjects = course.getCanceledSubjects();
+		Course.print(cSubjects);
 	}
 
 }
 
+class Course {
+	private Set<String> subjects;
+	private List<Student> students;
+	
+	public void setStudents(Student... arrStudent) {
+		students = new ArrayList<>();
+		Collections.addAll(students, arrStudent);
+	}
+	
+	public List<Student> getStudents() {
+		return students;
+	}
+	
+	public void setSubjects(String[] arrSubjects) {
+		List<String> list = new ArrayList<>();
+		Collections.addAll(list, arrSubjects);
+		subjects = new HashSet<>(list);
+	}
+	
+	public Set<String> getSubjects() {
+		return subjects;
+	}
+	
+	public List<String> getOrderedSubjects() {
+		List<String> oSubjects = new ArrayList<>(subjects);
+		Collections.sort(oSubjects);
+		return oSubjects;
+	}
+	
+	public Set<String> getRegisteredSubjects() {
+		Set<String> rSubjects = new HashSet<>();
+		for (Student student : students) {
+			rSubjects.addAll(student.getSubjects());
+		}
+		return rSubjects;
+	}
+	
+	public Set<String> getBasicSubjects() {		
+		Set<String> sbj1 = null;
+		Set<String> sbj2 = null;
+		
+		for (int i=0; i<students.size()-1; i++) {
+			sbj1 = new HashSet<>(students.get(i).getSubjects());
+			sbj2 = new HashSet<>(students.get(i+1).getSubjects());
+			
+			sbj1.retainAll(sbj2);
+		}
+		
+		return sbj1;
+	}
+	
+	public Set<String> getCanceledSubjects() {
+		Set<String> allSubjects = new HashSet<>(subjects);
+		Set<String> rSubjects = getRegisteredSubjects();
+		
+		allSubjects.removeAll(rSubjects);
+		
+		return allSubjects;
+	}
+	
+	public static void print(Collection<String> subjects) {
+		for (String subject : subjects) {
+			System.out.print(subject + ", ");
+		}
+		System.out.println("\n");
+	}
+}
+
 class Student {
 	private String name;
-	private Collection<String> books;
+	private Set<String> subjects;
 	
-	public Student(String name, Collection<String> books) {
+	public Student(String name) {
 		this.name = name;
-		this.books = books;
+		subjects = new HashSet<>();
 	}
+	
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
+	
+	public void addSubject(String subject) {
+		subjects.add(subject);
 	}
-	public Collection<String> getBooks() {
-		return books;
-	}
-	public void setBooks(Collection<String> books) {
-		this.books = books;
-	}
-	public void addBook(String book) {
-		books.add(book);
-	}
-	@Override
-	public String toString() {
-		return "Student [name=" + name + ", books=" + books + "]";
+	
+	public Set<String> getSubjects() {
+		return subjects;
 	}
 }
 
---
+++ src/com/lcomputerstudy/books/java/ch18/Ex03Set.java
@@ -0,0 +1,10 @@
+package com.lcomputerstudy.books.java.ch18;
+
+public class Ex03Set {
+
+	public static void main(String[] args) {
+		// 링크드 해시셋, 트리셋
+
+	}
+
+}
