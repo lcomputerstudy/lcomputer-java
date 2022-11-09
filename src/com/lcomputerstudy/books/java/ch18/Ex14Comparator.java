15fa3250fd0728056dbad45aac063ec23e1360aa
LcomputerStudy <lcomputerstudy@naver.com>
2022-11-09 22:44:48 +0900

    Comparator 문제 추가

--- src/com/lcomputerstudy/books/java/ch18/Ex14Comparator.java
+++ src/com/lcomputerstudy/books/java/ch18/Ex14Comparator.java
@@ -108,4 +108,10 @@ class Book4 implements Comparable<Book4> {
 문제 2.
 번호 내림차순, 이름 내림차순으로 정렬하는 Comparator 를 이용해 출력하세요.
 Comparator 는 익명클래스로 작성하세요.
+
+문제 3.
+PriorityQueue에 책들을 입력하세요.
+Queue에서 책의 가격이 높은 책 부터 제거되도록 코딩하세요.
+Comparable을 이용해 가격이 높은 책부터 제거되도록 작성하세요. 
+Comparator를 이용해 가격이 낮은 책부터 제거되도록 코딩하세요.
 */
---
+++ src/com/lcomputerstudy/books/java/ch18/Ex15Lambda.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2022. 이신만 <rngooqw@gmail.com> All rights reserved.
+ */
+package com.lcomputerstudy.books.java.ch18;
+
+public class Ex15Lambda {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
